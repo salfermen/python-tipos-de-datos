1b8c9246415ea4f806e34473b5784060854eb978
salfermen <salimferezmendez2005@gmail.com>
2023-08-15 14:03:55 -0500

    Se agregó la operación de Módulo y Potencia

--- Calculadora.py
+++ Calculadora.py
@@ -3,7 +3,7 @@ print("Ingrese el número 1")
 numero1 = int(input())
 print("Ingrese el número 2")
 numero2 = int(input())
-print("Ingrese la operación (+, -, *, /)")
+print("Ingrese la operación (+, -, *, /, ^, %)")
 operacion = input()
 
 match operacion:
@@ -23,5 +23,11 @@ match operacion:
         # Operación división:
         division = numero1 / numero2
         print("La división es " + str(division))
+    case'^':
+        power = numero1 ** numero2
+        print("la potencia es de "+ str(power))
+    case'%':
+        mod = numero1 % numero2
+        print('El módulo es de ' + str(mod))
     case _ :
         print("Operación inválida")
